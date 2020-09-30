850f57f856332f1b66243c21fc171b062d36e270
notedop <raoul2405@hotmail.com>
2020-09-30 20:31:04 +0200

    enhance ConfigCollection methods
    implementing 2 additional methods:
    - countConfigByName: Allows to count number of occurences filtered by name
    - getConfigByNameAndId: Gets the first config class by name and id

--- src/main/java/com/rvh/openoffice/parts/config/ConfigCollection.java
+++ src/main/java/com/rvh/openoffice/parts/config/ConfigCollection.java
@@ -2,6 +2,8 @@ package com.rvh.openoffice.parts.config;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 public class ConfigCollection<t extends Config> {
 
@@ -22,5 +24,16 @@ public class ConfigCollection<t extends Config> {
 
     public void removeConfig (String name) {
         configs.removeIf(config -> config.getName().equals(name));
+    }
+
+    public long countConfigByName(String name) {
+        Predicate<t> byName = config -> config.getName().equals(name);
+        return configs.stream().filter(byName).count();
+    }
+
+    public t getConfigByNameAndId(String name, String relId){
+        Predicate<t> byNameAndId = config -> config.getName().equals(name) && config.getId().equals(relId);
+        Optional<t> item = configs.stream().findFirst().filter(byNameAndId);
+        return item.orElse(null);
     }
 }
